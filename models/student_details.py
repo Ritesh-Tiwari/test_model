368f03845844f24109cb7e13907856ea548fbba0
DesiSwag <62049064+Ritesh-Tiwari@users.noreply.github.com>
2020-03-31 19:19:03 +0530

    changes in import lines

--- models/student_details.py
+++ models/student_details.py
@@ -1,16 +1,15 @@
-import server.odoo.fields
-import server.odoo.models
+from odoo import fields ,models
 
 
-class StudentDetails(server.odoo.models.Model):
-    _name = 'student_details'
+class StudentDetails(models.Model):
+    _name = 'student.details'
     _rec_name = 'name'
     _description = 'Details of Students'
 
-    name = server.odoo.fields.Char(string="Student Name")
-    dob = server.odoo.fields.Date(string="Date Of Birth", required=False)
-    age = server.odoo.fields.Integer(string="Age", required=False)
-    standard = server.odoo.fields.Integer(str="Standard", required=False)
-    division = server.odoo.fields.Integer(string="Division", required=False)
-    father = server.odoo.fields.Char(string="Father", required=False)
-    mother = server.odoo.fields.Char(string="Mother", required=False)
+    name = fields.Char(string="Student Name")
+    dob = fields.Date(string="Date Of Birth", required=False)
+    age = fields.Integer(string="Age", required=False)
+    standard = fields.Integer(str="Standard", required=False)
+    division = fields.Integer(string="Division", required=False)
+    father = fields.Char(string="Father", required=False)
+    mother = fields.Char(string="Mother", required=False)
